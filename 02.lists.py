5019959bea284e855d1a5cfd2daa7867693e715d
ь <das.lozh19@mail.ru>
2020-09-14 11:04:44 +0500

    lists

--- 02.lists.py
+++ 02.lists.py
@@ -7,3 +7,12 @@ while True:
         break
 print('ввод завершен')
 print(student_marks)
+
+i = 0
+avg_mark = 0
+while i < len (student_marks):
+    # print(type(avg_mark), type(student_marks[i]))
+    avg_mark += int(student_marks[i])
+    i += 1
+avg_mark /= len(student_marks)
+print (avg_mark)
