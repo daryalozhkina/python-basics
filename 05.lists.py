aac9ac91dfdc04c4f05126552a7cb78990a02818
ь <das.lozh19@mail.ru>
2020-09-14 12:38:06 +0500

    enumerate

--- 05.lists.py
+++ 05.lists.py
@@ -7,6 +7,25 @@ student_marks = [5, 4, 3, 2, 5]
 #while i < len(student_marks):
    # print(lesson_dates[i], 'оценка', student_marks[i])
    # i += 1
+for item in student_marks:
+     print('оценка', item)
 
-for mark in student_marks:
-    print('оценка', mark)
+
+for item in enumerate(student_marks):
+     print('оценка', item)
+     i, mark = item
+     print('оценка', item, 'или', i, mark)
+
+     for i, mark in enumerate(student_marks):
+         print('оценка', i,mark)
+  #  print(lesson_dates())
+
+#user_full_name = ['Иван', 'Иванов']
+
+
+#user_full_name = ['Иван', 'Иванов']
+#first_name, second_name = user_full_name[0]
+#print(first_name, second_name)
+
+#a, b, c, d, e = [15, 45, 87, 96, 4]
+#print(a, b, c, d, e)
