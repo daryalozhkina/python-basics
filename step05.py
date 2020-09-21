8eb167e044945e567c123338426b3cff65f9a14d
ь <das.lozh19@mail.ru>
2020-09-21 11:41:49 +0500

    step05

---
+++ step05.py
@@ -0,0 +1,21 @@
+a = 'всем привет'
+print(type(a))
+print(dir(a))
+
+print(a.isdigit())
+
+b = '15.7'
+print(b.isdigit())
+
+c = '157'
+print(c.isdigit())
+
+e = '15e7'
+print(e.isdigit())
+
+avg_mark = input ('введите средний балл студента: ')
+if avg_mark.isdigit():
+    avg_mark = float (avg_mark)
+    print ('ввод корректен', type(avg_mark), avg_mark)
+except ValueError:
+    print('некорректное значение', avg_mark)
