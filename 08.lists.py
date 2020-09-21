a2d81b624c31edb0852eb63b174917787b76bff1
ь <das.lozh19@mail.ru>
2020-09-21 11:05:49 +0500

    step03

---
+++ 08.lists.py
@@ -0,0 +1,27 @@
+student_marks = []
+while True:
+    mark = input('введите оценку студента:\n')
+    if not mark:
+        break
+    if len(mark) != 1:
+        print('вы ввели не один символ')
+        continue
+   # if not 48 <- ord(mark) <- 57:
+    #    print('вы ввели не число')
+    #    continue
+    try:
+        mark = int(mark)
+        if 1 <= mark <= 5:
+             student_marks.append(mark)
+        elif mark > 5:
+            print('Оценка больше 5')
+        elif mark < 1:
+            print('Оценка больше 1')
+    except ValueError as e:
+        print('ошибка значения', e)
+    except TypeError as e:
+        print('ошибка значения', e)
+    except Exception as e:
+        print('была допущена ошибка', e)
+
+print('ввод завершен', student_marks)
