ac5f2d124e1edbb74ce1f10cd023bf81bedd3a07
ь <das.lozh19@mail.ru>
2020-09-21 10:19:13 +0500

    elif

--- 07.lists.py
+++ 07.lists.py
@@ -1,14 +1,15 @@
 student_marks = []
 while True:
     mark = input('введите оценку студента:\n')
-    if mark:
-        mark = int(mark)
-        if 1 <= mark <= 5:
-            student_marks.append(mark)
-        else:
-            print("Оценка не подходит под категорию оценивания")
-    else:
+    if not mark:
         break
+    mark = int(mark)
+    if 1 <= mark <= 5:
+            student_marks.append(mark)
+    elif mark > 5:
+        print('Оценка больше 5')
+    elif mark < 1:
+        print('Оценка больше 1')
 
 print('ввод завершен')
 
