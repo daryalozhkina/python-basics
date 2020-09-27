ce60c9028084afc58560c98dddce2d9eb63642a0
ь <das.lozh19@mail.ru>
2020-09-27 13:30:22 +0500

    step06

--- 200921/step06.py
+++ 200921/step06.py
@@ -1,12 +1,16 @@
-class_pupile = input('введите имена учеников через запятую')
+#class_pupile = input('введите имена учеников через запятую')
 # Полина, Антон, Арсений, Евгений, Алексей, Тимур
-class_pupile = 'Полина, Антон, Арсений, Евгений, Алексей, Тимур'
+class_pupils = 'Полина, Антон, Арсений, Евгений, Алексей, Тимур'
 correct_result = ['Полина', 'Антон', 'Арсений', 'Евгений', 'Алексей', 'Тимур']
 #print('ученики класса:', class_pupile)
 
-result = class_pupils.split(',')
+_result = class_pupils.split(', ')
+result = []
+for item in _result:
+    #print(item, '->', item.strip())
+    result.append(item.strip())
 
 assert result == correct_result, 'алгоритм реализован неверно'
 #if not result == correct_result:
  #   ralse result == correct_result, 'алгоритм реализован неверно'
-print(result)
+#print(result)
