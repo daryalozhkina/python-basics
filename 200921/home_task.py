7770fc2a69a622af2879c3ddf1ecfd93dea01944
ь <das.lozh19@mail.ru>
2020-09-27 17:14:13 +0500

    home-tasks

---
+++ 200921/home_task.py
@@ -0,0 +1,10 @@
+row_record = '217.168.17.5 - - [17/May/2015:08:05:12 +0000] "GET /downloads/product_2 HTTP/1.1" 200 3316 "-" "-"'
+
+correct_requests_IP_address = '217.168.17.5'
+correct_requests_date = '17/May/2015:08:05:12 +0000'
+requests_IP_address = row_record.split() [3]
+requests_date = row_record.split() [0]
+print(requests_IP_address, requests_date)
+
+assert requests_IP_address == correct_requests_IP_address
+assert requests_date == correct_requests_date
