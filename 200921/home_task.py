380f1e887e08d6d93a61a9e05e7d2c42e276d59b
ь <das.lozh19@mail.ru>
2020-09-27 17:41:43 +0500

    home_tasks

--- 200921/home_task.py
+++ 200921/home_task.py
@@ -2,8 +2,8 @@ row_record = '217.168.17.5 - - [17/May/2015:08:05:12 +0000] "GET /downloads/prod
 
 correct_requests_IP_address = '217.168.17.5'
 correct_requests_date = '17/May/2015:08:05:12 +0000'
-requests_IP_address = row_record.split() [3]
-requests_date = row_record.split() [0]
+requests_IP_address = row_record.split() [0]
+requests_date = row_record.split() [3]
 print(requests_IP_address, requests_date)
 
 assert requests_IP_address == correct_requests_IP_address
