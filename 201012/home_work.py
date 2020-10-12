21a827ffd8f7e1fb5ac4276ab7a6fddf104dd8d9
ь <das.lozh19@mail.ru>
2020-10-12 09:34:31 +0500

    201012  -numbers

---
+++ 201012/home_work.py
@@ -0,0 +1,54 @@
+class People: # upper CamelCase
+    def __init__(self):
+        self.surname = None
+        self.name = None
+        self.patronymic = None
+        self.birthdate = None
+        self.group = None
+
+
+class People:
+    def __init__(self):
+        self.surname = None
+        self.name = None
+        self.patronymic = None
+        self.birthdate = None
+        self.group = None
+
+class Teacher(People):
+    def __init__(self):
+        self.surname = None
+        self.name = None
+        self.patronymic = None
+        self.discipline = None
+
+
+class Group:
+    def __init__(self):
+        self.specialty = None
+        self.number_of_students = None
+        self.patronymic = None
+        self.curator = None
+
+
+class College:
+    def __init__(self):
+        self.administration = None
+        self.teacher = None
+        self.training_group = None
+        self.number_of_students = None
+        self.discipline = None
+
+
+class Examination:
+    def __init__(self):
+        self.teacher = None
+        self.discipline = None
+        self.mark = None
+
+
+class Student_on_the_exam:
+    def __init__(self):
+        self.gradebook = None
+        self.discipline = None
+        self.mark = None
