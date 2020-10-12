46259672a7311d38d78cdb214cceec7711f1fb0c
ь <das.lozh19@mail.ru>
2020-10-12 11:19:07 +0500

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	201012/step_02.py

---
+++ 201012/step_05.py
@@ -0,0 +1,104 @@
+class People: # upper CamelCase
+    def __init__(self):
+        self.surname = None
+        self.name = None
+        self.patronymic = None
+        self.birthdate = None
+        self.group = None
+
+def say_hello(self):
+    print('hello, my name is', )
+
+
+
+class Student(People):
+    def __init__(self):
+    super()_ __init__()
+        self.surname = None
+        self.name = None
+        self.patronymic = None
+        self.birthdate = None
+        self.group = None
+
+class Teacher(People):
+    def __init__(self):
+        super()_ __init__()
+        self.surname = None
+        self.name = None
+        self.patronymic = None
+        self.discipline = None
+
+def say_speciality(self):
+class Group:
+    def __init__(self):
+        self.specialty = None
+        self.number_of_students = None
+        self.patronymic = None
+        self.curator = None
+
+
+class College:
+    def __init__(self):
+        self.administration = None
+        self.teacher = None
+        self.training_group = None
+        self.number_of_students = None
+        self.discipline = None
+
+
+class Examination:
+    def __init__(self):
+        self.teacher = None
+        self.discipline = None
+        self.mark = None
+
+
+class Student_on_the_exam:
+    def __init__(self):
+        self.gradebook = None
+        self.discipline = None
+        self.mark = None
+
+class HedTeaacher(Teacher):
+    def call_teachers(self):
+
+
+student_1 = Student()
+student_1.say_hello()
+student_1.say_group()
+
+teacher_1 = Student()
+teacher_1.say_hello()
+
+
+
+
+
+# class WaterElemental:
+#     def create_river(self):
+#         print('here is the river')
+#
+# class FireElemental:
+#     def create_firewall(self):
+#         print('firewall created')
+#
+# class EarthElemental:
+#     def create_earthquake(self):
+#         print('dig earthquare happens')
+#
+# class SuperHero(WaterElemental, FireElemental, EarthElemental):
+#         pass
+# # class ColledgePeople(AbstractUser):
+# #     pass
+# class SuperHero(WaterElemental, FireElemental, EarthElemental):
+#     def create_waterfall(self):
+#         print('waterfall sounds')
+#
+#
+# super_hero_1 = SuperHero()
+# print(type(super_hero_1))
+# super_hero_1.create_river()
+# super_hero_1.create_firewall()
+#
+# water_elemental_1 = WaterElemental()
+# print()
